e37a78e642bb6feaa14f059aed983765a087e990
nchigumbura <nchigumbura@byupathway.edu>
2025-03-24 18:48:33 +0200

    commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,11 +1,9 @@
-// Footer Date
 const yearElement = document.getElementById("year");
 yearElement.textContent = new Date().getFullYear();
 
 const lastModifiedElement = document.getElementById("last-modified");
 lastModifiedElement.textContent = document.lastModified;
 
-// Hamburger Menu Functionality
 const menuButton = document.querySelector(".hamburger-button");
 const navMenu = document.querySelector("nav");
 
@@ -13,3 +11,18 @@ menuButton.addEventListener("click", () => {
   navMenu.classList.toggle("visible");
   menuButton.textContent = navMenu.classList.contains("visible") ? "X" : "☰";
 });
+
+function calculateWindChill(temp, windSpeed) {
+  return Math.round(
+    13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)
+  );
+}
+
+const temp = 10;
+const windSpeed = 5;
+
+if (temp <= 10 && windSpeed > 4.8) {
+  document.getElementById("windchill").textContent = calculateWindChill(temp, windSpeed) + " °C";
+} else {
+  document.getElementById("windchill").textContent = "N/A";
+}
